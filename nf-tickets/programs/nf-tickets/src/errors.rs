460874f555cb3823f0aa2ca7bd1d634fc2970577
Allen-Saji <allensaji04@gmail.com>
2024-09-18 15:34:17 +0530

    add ticket errors

--- nf-tickets/programs/nf-tickets/src/errors.rs
+++ nf-tickets/programs/nf-tickets/src/errors.rs
@@ -5,3 +5,36 @@ pub enum NameError {
     #[msg("Name must be between 1 and 32 characters")]
     NameTooLong,
 }
+
+#[error_code]
+pub enum TicketError {
+    #[msg("The maximum number of tickets for this event has been reached")]
+    MaximumTicketsReached,
+
+    #[msg("Numerical overflow occurred")]
+    NumericalOverflow,
+
+    #[msg("Required attribute is missing")]
+    MissingAttribute,
+
+    #[msg("Invalid ticket price")]
+    InvalidPrice,
+
+    #[msg("Unauthorized operation")]
+    Unauthorized,
+
+    #[msg("Event has already started")]
+    EventStarted,
+
+    #[msg("Ticket is already used")]
+    TicketAlreadyUsed,
+
+    #[msg("Invalid ticket status")]
+    InvalidTicketStatus,
+
+    #[msg("Event not found")]
+    EventNotFound,
+
+    #[msg("Ticket not found")]
+    TicketNotFound,
+}
